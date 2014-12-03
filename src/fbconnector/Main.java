9f8b2cea6c304984d6234999a285f8b0fc895a2b
Petr Škola <skola.petr@email.cz>
2014-12-03 23:39:00 +0100

    inicial - stahuje fb feed a commenty, a rovnou je posílá do es
    je nutné použít vlastní token z FB.

---
+++ src/fbconnector/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fbconnector;
+
+/**
+ *
+ * @author Petr
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GraphReader graph = new GraphReader("TOKEN");
+    }
+}
